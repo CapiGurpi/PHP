a8723bf3d54163d957a780921a096b5d3472772b
CapiGurpi <gurpicapitan@gmail.com>
2026-02-10 13:26:16 +0100

    P4 add

--- P4/Exercise01.php
+++ P4/Exercise01.php
@@ -1,40 +1,76 @@
+<!DOCTYPE html>
 <?php
-$numero1 = 10;
-$numero2 = 20;
-$numero3 = 30;
-$numeros = [$numero1,$numero2,$numero3];
-
-echo "Current array: ";
-print_r($numeros);
-
-if (isset($_POST["position"]) && isset($_POST["numero1"])) {
-    $pos = (int)$_POST["position"];
-    $nuevo = (int)$_POST["numero1"];
-
-    $numeros[$pos] = $nuevo;
-}
-
+session_start();
 ?>
-<h2>Modify array saved in session</h2>
-<form action="Exercise01.php" method="post" enctype="multipart/form-data">
-    Position to modify:
-    <select name="position">
-        <option value="">0</option>
+<html>
+<body>
+<h1>Cambiar valores de un array</h1>
+<form action="Ejercicio1.php" method="post" enctype="multipart/form-data">
+    <select name="opciones">
+        <option value="">-- Selecciona --</option>
         <option value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
-    </select><br><br>
-
-
-    <form method="get">
-        New value:<br> <input type="int" name="numero1"><br>
-        <input type="submit" value="Submit">
-    </form>
-
-    <form method = "get">
-        <input type="submit" value="Modify"> <input type="submit" value="Average">  <input type="reset" value="Reset">
-    </form>
-
+</select><br><br>
+ 
+ 
+   
+  Nuevo valor <input type="number" name="valor"> <br> <br>
+   <input type="submit" name = "accion" value="Modificar">
+    <input type="submit" name = "accion"  value="Media">
+     <input type="reset" name = "accion"  value="Reiniciar">
+</form>
+<br> <br>
+<?php
+ 
+ 
+if (isset($_POST['opciones'])) {
+    $Opcion = htmlspecialchars($_POST['opciones']);
+}
+if (!isset($_POST['accion'])) {
+ 
+    $_SESSION["Array"] = array(1=>10, 2=>20, 3=> 30);
+}
+if (isset($_POST['accion'])) {
+$valor = (int)$_POST['valor'];
+   
+    switch ($_POST['accion']) {
+        case 'Modificar':
+           
+           
+            $_SESSION["Array"][$Opcion]=$valor;
+           
+            break;
+ 
+        case 'Media':
+            $Media=0;
+            foreach ($_SESSION["Array"] as $x) {
+            $Media = $Media + $x;
+ 
+                }
+            echo "<br>";
+           
+            $Media = $Media / count($_SESSION["Array"]);
+            echo "Current Average: ". $Media;
+            echo "<br>";
+            echo "<br>";
+                break;
+ 
+ 
+       
+    }
+}
+foreach ($_SESSION["Array"] as $x) {
+  echo $x," ";
+ 
+}
+echo "<br>";
+echo "<br>";
+ 
+ 
+?>
+</body>
+ 
     
 
 
